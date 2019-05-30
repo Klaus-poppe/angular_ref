4d94a69c5421acc11b03e7b21ec1a01cac1dae6e
Klaus-poppe <vesparadkt@gmail.com>
2019-05-30 17:35:33 +0530

    node modules added + tslint + tsconfig

--- src/app/randi/randi.component.ts
+++ src/app/randi/randi.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-randi',
   templateUrl: './randi.component.html',
-  styleUrls: ['./randi.component.css']
 })
 export class RandiComponent implements OnInit {
 
